c526d6f97b05d2d22e2b229605a821124c27beef
Bongokuhle <zulucoding5223@gmail.com>
2026-01-07 15:39:30 +0200

    Fixed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // this is default
-  },
-  base: "/", // make sure this matches your deployment path
-});
+  plugins: [react(),
+    tailwindcss(),
+  ],
+})
+
+
