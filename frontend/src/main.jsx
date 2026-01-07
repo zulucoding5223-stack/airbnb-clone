494e90079bde82de08e1bed747a9a3ed70d1da54
Bongokuhle <zulucoding5223@gmail.com>
2026-01-08 00:19:10 +0200

    Deploy frontend to GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./utils/AppContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/airbnb-clone">
     <AppContextProvider>
       <App />
     </AppContextProvider>
