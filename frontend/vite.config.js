5610859f0a74d8fcff5d8ff0df21528d8b9e66ba
Bongokuhle <zulucoding5223@gmail.com>
2026-01-08 10:46:27 +0200

    Set base path for GitHub Pages deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/airbnb-clone/",
   plugins: [react(),
     tailwindcss(),
-    
   ]
 })
 
