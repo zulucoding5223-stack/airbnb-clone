03fa110d43f1c5bef90587343f5a8ab159ccf528
Bongokuhle <zulucoding5223@gmail.com>
2026-01-08 08:54:53 +0200

    Added HashRouter to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import AppContextProvider from "./utils/AppContextProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/airbnb-clone">
+  <HashRouter>
     <AppContextProvider>
       <App />
     </AppContextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
